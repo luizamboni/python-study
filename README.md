ae68e1e6bb82b93cdaff0941297201526779f929
Luiz Zamboni <luizamboni2002@hotmail.com>
2018-06-20 23:33:59 -0300

    INIT number

---
+++ README.md
@@ -0,0 +1,4 @@
+Python Study
+===
+
+This is a collection of little projects with intent to make me more smart in Python environment
---
+++ numbers/1-operations.py
@@ -0,0 +1,24 @@
+
+# sum
+print("1+1:", 1 + 1)
+
+# subtraction
+print("1-3:", 1 - 3)
+
+
+# potence
+print("2^2:", 2 ** 2)
+
+print("2^(2^(2)):", 2 ** 2 ** 2)
+
+# root
+print("sqrt 4:", 4 ** (1/2))
+
+print("sqrt 2:", 2 ** (1/2))
+
+print("3 sqrt 9:", 27 ** (1/3))
+
+
+# rest of division
+print("3 % 2:", 3 % 2 )
+print("4 % 2:", 4 % 2 )
